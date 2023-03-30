f13c0da318124835739cd654d074e16e1648e567
David Knowles <dknowles2@gmail.com>
2023-03-30 23:17:47 +0000

    Index grills by name.

--- scripts/dump_grills.py
+++ scripts/dump_grills.py
@@ -18,7 +18,7 @@ import requests
 
 logging.basicConfig(level=logging.DEBUG)  # Log all HTTP requests to stderr.
 API_URL = "https://api-prod.dansonscorp.com/api/v1"
-MODELS = ("LBL", "LFS", "PBL", "PBP", "PBC", "PBM", "PBV", "PBG")
+CONTROL_BOARDS = ("LBL", "LFS", "PBL", "PBP", "PBC", "PBM", "PBV", "PBG")
 
 
 def login(username, password):
@@ -33,12 +33,16 @@ def login(username, password):
 
 
 def get_grill_details(grill_id, auth):
+    logging.info("Fetching grill details for grill_id: %s", grill_id)
     resp = requests.get(API_URL + f"/grills/{grill_id}", headers=auth)
     return resp.json()["data"]["grill"]
 
 
-def get_model_grills(model, auth):
-    resp = requests.get(API_URL + f"/grills?control_board={model}", headers=auth)
+def get_control_board_grills(control_board, auth):
+    logging.info("Fetching grills for control_board: %s", control_board)
+    resp = requests.get(
+        API_URL + f"/grills?control_board={control_board}", headers=auth
+    )
     for grill in resp.json()["data"]["grills"]:
         yield get_grill_details(grill["id"], auth)
 
@@ -48,12 +52,12 @@ def main():
     cfg = configparser.ConfigParser()
     cfg.read(str(Path.home() / ".pitboss"))
     auth = login(cfg["pitboss"]["username"], cfg["pitboss"]["password"])
-    grills = []
-    for model in MODELS:
-        for grill in get_model_grills(model, auth):
-            grills.append(grill)
+    grills = {}
+    for control_board in CONTROL_BOARDS:
+        for grill in get_control_board_grills(control_board, auth):
+            grills[grill["name"]] = grill
 
-    print(json.dumps({"grills": grills}, indent=2, sort_keys=True))
+    print(json.dumps(grills, indent=2, sort_keys=True))
 
 
 if __name__ == "__main__":
