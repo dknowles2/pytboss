a757bad94f1b88387f21df88b2ac25d1a045f483
David Knowles <dknowles2@gmail.com>
2025-12-01 13:30:10 +0000

    Convert scripts to async

--- scripts/fetch_firmware.py
+++ scripts/fetch_firmware.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Script that fetches the newest firmware files for a grill model.
 
 Application credentials should be stored in a file called ".pitboss" in your
@@ -7,68 +7,67 @@ home directory. The format is an INI style like this:
 [pitboss]
 username = email@address.com
 password = my-secret-password
+
+Run with python3 -m scripts.fetch_firmware <firmware_output_dir>
 """
 
 import configparser
 import logging
-from pathlib import Path
 import sys
+from asyncio import run
+from pathlib import Path
+from types import SimpleNamespace
+from typing import Any
 
-import requests
+from aiohttp import ClientSession, TraceConfig, TraceRequestStartParams
+
+from pytboss.auth import async_login
 
 logging.basicConfig(level=logging.DEBUG)  # Log all HTTP requests to stderr.
+
 API_URL = "https://api-prod.dansonscorp.com/api/v1"
 CONTROL_BOARD_ID = 5
 CONTROL_BOARD_NAME = "PBV"
 
 
-def login(username, password):
-    params = {"email": username, "password": password}
-    resp = requests.post(API_URL + "/login/app", params=params)
+async def _onr_request_start(
+    session: ClientSession, context: SimpleNamespace, params: TraceRequestStartParams
+) -> None:
+    logging.getLogger("aiohttp.client").debug("%s %s", params.method, params.url)
+
+
+async def get_grills(session: ClientSession) -> list[dict[str, Any]]:
+    resp = await session.get(f"{API_URL}/customer-grills")
     resp.raise_for_status()
-    # Example response:
-    # {
-    #      "status": "success",
-    #      "message": null,
-    #      "errors": null,
-    #      "data": {
-    #          "token": "xxx",
-    #          "token_expiration": YYYY-MM-DDTHH:MM:SSZ"
-    #      }
-    # }
-    token = resp.json()["data"]["token"]
-    return {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {token}",
-    }
+    return (await resp.json())["data"]["customer_grills"]
 
 
-def get_grills(auth):
-    resp = requests.get(API_URL + "/customer-grills", headers=auth)
-    resp.raise_for_status()
-    return resp.json()["data"]["customer_grills"]
-
-
-def get_firmware_grill(auth, control_board_name):
-    resp = requests.get(
-        API_URL + "/grills", headers=auth, params={"control_board": control_board_name}
+async def get_firmware_grill(
+    session: ClientSession, control_board_name: str
+) -> dict[str, Any]:
+    resp = await session.get(
+        f"{API_URL}/grills", params={"control_board": control_board_name}
     )
     resp.raise_for_status()
-    return resp.json()["data"]["grills"][0]
+    return (await resp.json())["data"]["grills"][0]
 
 
-def get_firmware_metadata(auth, grill):
+async def get_firmware_metadata(
+    session: ClientSession, grill: dict[str, Any]
+) -> dict[str, Any]:
     control_board_id = grill["id"]
-    resp = requests.get(
-        API_URL + f"/firmware-platforms/{control_board_id}", headers=auth
-    )
+    resp = await session.get(f"{API_URL}/firmware-platforms/{control_board_id}")
     resp.raise_for_status()
-    return resp.json()["data"]["firmwarePlatform"]
+    return (await resp.json())["data"]["firmwarePlatform"]
 
 
-def fetch_firmware(auth, control_board_name, grill, output_dir):
-    metadata = get_firmware_metadata(auth, grill)
+async def fetch_firmware(
+    session: ClientSession,
+    control_board_name: str,
+    grill: dict[str, Any],
+    output_dir: Path,
+) -> None:
+    metadata = await get_firmware_metadata(session, grill)
     firmware_base_url = metadata["firmware_base_url"]
     firmware_version_path = metadata["firmware_version"].replace(".", "-")
     for file in metadata["platform_files"]:
@@ -78,27 +77,33 @@ def fetch_firmware(auth, control_board_name, grill, output_dir):
         if file["board_specific"] > 0:
             filename = f"{fp.stem}_{control_board_name}{fp.suffix}"
         url += filename
-        (output_dir / fp).write_text(fetch_file(url))
+        (output_dir / fp).write_text(await fetch_file(session, url))
 
 
-def fetch_file(url):
-    resp = requests.get(url)
+async def fetch_file(session: ClientSession, url: str) -> str:
+    resp = await session.get(url)
     resp.raise_for_status()
-    return resp.text
+    return await resp.text()
 
 
-def main(argv):
+async def main(argv):
     if len(argv) < 2:
         print("Must provide an output path for firmware files", file=sys.stderr)
         sys.exit(1)
     cfg = configparser.ConfigParser()
     cfg.read(str(Path.home() / ".pitboss"))
-    auth = login(cfg["pitboss"]["username"], cfg["pitboss"]["password"])
-    grills = get_grills(auth)
-    control_board_name = grills[0]["board_id"].split("-")[0]
-    firmware_grill = get_firmware_grill(auth, control_board_name)
-    fetch_firmware(auth, control_board_name, firmware_grill, Path(argv[1]))
+    tc = TraceConfig()
+    tc.on_request_start.append(_onr_request_start)
+    async with ClientSession(trace_configs=[tc]) as session:
+        auth_headers = await async_login(
+            session, cfg["pitboss"]["username"], cfg["pitboss"]["password"]
+        )
+    async with ClientSession(headers=auth_headers, trace_configs=[tc]) as session:
+        grills = await get_grills(session)
+        control_board_name = grills[0]["board_id"].split("-")[0]
+        firmware_grill = await get_firmware_grill(session, control_board_name)
+        await fetch_firmware(session, control_board_name, firmware_grill, Path(argv[1]))
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    run(main(sys.argv))
